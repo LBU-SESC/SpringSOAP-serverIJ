f09ea2083f07f80e5d605cdcad5c2a55b6770fb0
Duncan Mullier <D.Mullier@leedsbeckett.ac.uk>
2024-02-14 17:08:26 +0000

    Initial commit

---
+++ src/main/java/com/example/producingwebservice/SpringSoapServer24IjApplication.java
@@ -0,0 +1,13 @@
+package com.example.producingwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapServer24IjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapServer24IjApplication.class, args);
+	}
+
+}
